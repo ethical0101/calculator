707039c00f34c74177a9adfac95b7f28d1ff72b5
MAD_DRAX <123973965+ethical0101@users.noreply.github.com>
2023-10-30 15:20:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-<script>
-    document.addEventListener("DOMContentLoaded", function () {
-        const inputField = document.querySelector("input");
-        const buttons = document.querySelectorAll("button");
+// Get references to the HTML elements
+const input = document.querySelector('input');
+const buttons = document.querySelectorAll('button');
 
-        buttons.forEach(function (button) {
-            button.addEventListener("click", function () {
-                const buttonText = button.textContent;
+// Add click event listeners to the buttons
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const buttonText = button.innerText;
 
-                if (buttonText === "AC") {
-                    inputField.value = "";
-                } else if (buttonText === "DEL") {
-                    inputField.value = inputField.value.slice(0, -1);
-                } else if (buttonText === "+/-") {
-                    inputField.value = eval(inputField.value) * -1;
-                } else if (buttonText === "=") {
-                    try {
-                        inputField.value = eval(inputField.value);
-                    } catch (error) {
-                        inputField.value = "Error";
-                    }
-                } else {
-                    inputField.value += buttonText;
-                }
-            });
-        });
-    });
-</script>
+    if (buttonText === 'AC') {
+      // Clear the input field
+      input.value = '';
+    } else if (buttonText === 'DEL') {
+      // Delete the last character
+      input.value = input.value.slice(0, -1);
+    } else if (buttonText === '=') {
+      try {
+        // Calculate the result and display it
+        input.value = eval(input.value);
+      } catch (error) {
+        input.value = 'Error';
+      }
+    } else {
+      // Append the button text to the input field
+      input.value += buttonText;
+    }
+  });
+});
